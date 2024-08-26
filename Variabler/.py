3e142bb480ffcbccc114655b4a2f51e3922cf726
OdinSund <odinbirgersund@gmail.com>
2024-08-26 13:55:25 +0200

    First commit

---
+++ Variabler/.py
@@ -0,0 +1,16 @@
+print ('Hello World')
+
+navn = input('Skriv inn navnet ditt: ')
+print ('Hello', navn)
+
+a = 10
+b = 20
+print(a)
+print(b)
+
+navn2 = input('Skriv inn ett nytt navn: ')
+sammen = (navn + navn2)
+print(sammen)
+
+sammen = (navn +' og ' + navn2)
+print(sammen)
